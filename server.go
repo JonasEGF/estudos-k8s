278bc1bff8b4469f2ad502728257aa0fa2e3a86e
Jonas Ferreira <jonas.edu@hotmail.com>
2021-06-20 20:09:14 -0300

    5-3

--- server.go
+++ server.go
@@ -6,10 +6,14 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
+
+var startedAt = time.Now()
 
 func main() {
 	http.HandleFunc("/", Hello)
+	http.HandleFunc("/healthz", Healthz)
 	http.HandleFunc("/configmap", ConfigMap)
 	http.HandleFunc("/secret", Secret)
 	http.ListenAndServe(":8080", nil)
@@ -27,7 +31,7 @@ func ConfigMap(w http.ResponseWriter, r *http.Request) {
 
 	data, err := ioutil.ReadFile("myfamily/family.txt")
 	if err != nil {
-		log.Fatalf("Error reading file: ", err)
+		log.Fatal("Error reading file: ", err)
 	}
 
 	fmt.Fprintf(w, "My family: %s", string(data))
@@ -39,4 +43,17 @@ func Secret(w http.ResponseWriter, r *http.Request) {
 	password := os.Getenv("PASSWORD")
 
 	fmt.Fprintf(w, "User: %s, Password: %s", user, password)
+}
+
+func Healthz(w http.ResponseWriter, r *http.Request) {
+
+	duration := time.Since(startedAt)
+
+	if duration.Seconds() > 25 {
+		w.WriteHeader(500)
+		w.Write([]byte(fmt.Sprint("Duration: #v", duration.Seconds())))
+	} else {
+		w.WriteHeader(200)
+		w.Write([]byte("ok"))
+	}
 }
